b380b88ef27bae7f641f65cc5e7fb3c4d6436f7a
osirispp <osiris.pena@conuco.do>
2014-05-05 00:23:27 -0400

    otra mas 05/05/2014

--- users/static/js/views/product.js
+++ users/static/js/views/product.js
@@ -33,10 +33,10 @@ Vendabyte.Views.Product = Backbone.View.extend({
             "user" : "Ramiro Fernandez",
             "date" : "26/5/2014"
         });
-        this.comments = new Vendabyte.Collections.Comments();
-        this.commentsView = new Vendabyte.Views.CommentList({ collection : this.comments, el : this.$el.children('section').children('.comment-cont') });  
-        this.comments.add(this.comment1); 
-        this.comments.add(this.comment2);    
+        //this.comments = new Vendabyte.Collections.Comments();
+        //this.commentsView = new Vendabyte.Views.CommentList({ collection : this.comments, el : this.$el.children('section').children('.comment-cont') });  
+        //this.comments.add(this.comment1); 
+        //this.comments.add(this.comment2);    
         //this.commentsView.render();
 		return this;
 	},
