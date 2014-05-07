c55c2d54e54d60e648be4224856954cca8bfef42
Carlos <carlos.sarante@conuco.do>
2014-05-06 23:00:46 -0400

    Se puede renderizar pagina, hay que probar los articulos

--- geographics/views.py
+++ geographics/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Province,City
 
-
-class CityView(viewsets.ModelViewSet):
-	model = City
-
-class ProvinceView(viewsets.ModelViewSet):
-	model = Province
