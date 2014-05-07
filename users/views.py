87ccc2cee5455714952daf63cfcbd6ac07b06bc4
Carlos <carlos.sarante@conuco.do>
2014-05-06 23:19:16 -0400

    Actualizado user.html

--- users/views.py
+++ users/views.py
@@ -7,20 +7,15 @@ from django.db.models import Count
 from .models import User
 #articles/new/ Renderiza los últimos posts usando la página articleblock.html en la variable articles, seguido del grupo a renderizar. 
 #Default: /articles/new/1 
-
-<<<<<<< HEAD
-
 def getUser(request):
     user = request.user
     articles = user.article_set.all()
     return render(request,'user.html',{'articles':articles})
-=======
 def getCurrentUserArticles(request):
 	user = request.user
 	articles = user.article_set.all()
 	return render(request,'user.html', {'articles':articles})
 
->>>>>>> 4e921c93a64bc43d2d88ff0a56d9c381dfa02bf4
 
 def loginUser(request):
 	username = request.POST['username']
