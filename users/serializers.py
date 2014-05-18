61bcbe4a5a66f5188bcc9ea68a01bd515c73822b
Carlos <carlos.sarante@conuco.do>
2014-05-18 01:12:15 -0400

    Actualizacion de usuarios.

--- users/serializers.py
+++ users/serializers.py
@@ -1,47 +1,17 @@
 import json
+from rest_framework import serializers
+from users.models import User,Badgets,Contact
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('first_name','last_name','birthday','username','photo','cover','city','rating','medals','following',)
 
 
-def serializeUser(queryset):
-	data = []
-	for user in queryset:
-		array = {
-			'id': user.id,
-			'username':user.username,
-			'first_name':user.first_name,
-			'last_name':user.last_name,
-			'birthday':('%s/%s/%s') % (user.birthday.year,user.birthday.month,user.birthday.day),
-			#'photo':user.photo.url,
-			#'cover':user.cover.url,
-			'city':user.city.city_name,
-			#'medals':serializeMedals(user.medals.all()),
-			#'following': serializeFollowers(user.follows.all()),
-		}	
-		data.append(array)
-	return json.dumps(data)
+class BadgetSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Badgets
 
-def serializeFollowers(queryset):
-	data = []
-	for follower in queryset:
-		f = {
-			'id': follower.id,
-			'username': follower.username,
-			'name': ('%s %s' % (follower.first_name,follower.last_name)),
-		}
-		data.append(f)
-	return json.dumps(data)
-
-
-def serializeMedals(queryset):
-	data = []
-	for medal in queryset:
-		array = {
-			'id':medal.id,
-			'medal_name':medal.medal.name,
-			'medal_icon':medal.medal_icon.url,
-		}
-		data.append(data)
-	return json.dumps(data)
-
-
-
-
+class ContactSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Contact
--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^me/(?P<response>\json)/$', 'users.views.getCurrentUser'),
-    url(r'^me/$', 'users.views.getCurrentUser'),
-    url(r'^me/articles/(?P<response>\json)/$', 'users.views.getCurrentUserArticles'), 
-    url(r'^me/articles/$', 'users.views.getCurrentUserArticles'),
-    url(r'^me/following/(?P<response>\json)/$', 'users.views.getCurrentUserFollows'),
+    url(r'^me/$', 'users.views.getUserIndex'),
     url(r'^me/following/$', 'users.views.getCurrentUserFollows'),
-    url(r'^me/followers/(?P<response>\json)/$', 'users.views.getCurrentUserFollowers'),
     url(r'^me/followers/$', 'users.views.getCurrentUserFollowers'),
-   #url(r'^me/followers/$', 'users.views.getCurrentUserFollowers'),
     url(r'^login/$', 'users.views.loginUser'),
-    #url(r'^(?P<username>[a-z0-9_-]{3,50})/$', 'users.views.getUser'),
+    url(r'^(?P<username>[a-z0-9_-]{3,50})/$', 'users.views.userManager'),
+    url(r'^(?P<pk>\d+)/$', 'users.views.userManager'),
 )
