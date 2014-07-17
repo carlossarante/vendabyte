4ba20a1f4aec577e378b47828386bc1024343705
Carlos <carlos.sarante@conuco.do>
2014-07-16 23:42:23 -0400

    Coreccion añadiendo permisos a los datos geograficos 2

--- geographics/views.py
+++ geographics/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 from .models import Province,City
 from .serializers import CitySerializer,ProvinceSerializer
