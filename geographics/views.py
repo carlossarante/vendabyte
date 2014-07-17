5482a04dd93ff03bc803e839ff4050a7b7a3bbe8
Carlos <carlos.sarante@conuco.do>
2014-07-16 23:40:10 -0400

    Coreccion añadiendo permisos a los datos geograficos

--- geographics/views.py
+++ geographics/views.py
@@ -9,9 +9,11 @@ from .serializers import CitySerializer,ProvinceSerializer
 class ProvinceSet(viewsets.ModelViewSet):
 	queryset = Province.objects.all()
 	serializer_class = ProvinceSerializer
+	permission_classes = (IsAuthenticatedOrReadOnly,)
 	filter_fields = ('province_name',)
 
 class CitySet(viewsets.ModelViewSet):
 	queryset = City.objects.all()
 	serializer_class = CitySerializer
 	filter_fields = ('city_name','province__province_name')
+	permission_classes = (IsAuthenticatedOrReadOnly,)
