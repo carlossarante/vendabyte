e432628ba97b57bc0e0ed0709c80b7b3ad927c87
Carlos <carlos.sarante@conuco.do>
2014-07-21 22:16:03 -0400

    Añadiendo el campo usuario.

--- users/models.py
+++ users/models.py
@@ -21,13 +21,14 @@ class User(AbstractBaseUser):
 	photo = models.ImageField(upload_to='users',blank=True)
 	cover = models.ImageField(upload_to='cover',blank=True)
 	city = models.ForeignKey(City,default=1)
+	username = models.CharField(max_length=255,unique=True)
 	medals = models.ManyToManyField(Badgets,related_name='medals',blank=True)
 	followers = models.ManyToManyField('self', related_name='follows', symmetrical=False,blank=True)
 	sex = models.CharField(max_length=10,null=True)
 	date_joined = models.DateTimeField(default=timezone.now)
 	objects = UserManager()
-	USERNAME_FIELD='email'
-	REQUIRED_FIELDS=['first_name','last_name','birthday',]
+	USERNAME_FIELD='username'
+	REQUIRED_FIELDS=['first_name','last_name','birthday','email']
 	is_active = models.BooleanField(default=True)
 	is_admin = models.BooleanField(default=False)
 	is_superuser = models.BooleanField(default=False)
