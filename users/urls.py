380241d4f80d73df86ad9c601d40453aaf9469cc
Carlos <carlos.sarante@conuco.do>
2015-01-13 00:46:58 -0400

    Bugcheck: Aceptando puntos

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
 	url(r'^login/$', 'users.views.loginFacebookUser'),
 	url(r'^logout/$', 'users.views.logout_me'),
 	url(r'^check_user/$', 'users.views.checkUserExistence'),
-    url(r'^(?P<username>[a-z0-9_-]{3,16})/$', 'users.views.userIndex'),
+    url(r'^(?P<username>[a-z0-9_-.]{3,16})/$', 'users.views.userIndex'),
 )   
